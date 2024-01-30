27d2cd09c427c3e72ef5d96b22c8fd707693a6dc
Ernani Ávila <ernanicavilaneto@gmail.com>
2024-01-30 10:40:26 -0300

    feat: start project with initial input and list todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todo, setTodo] = React.useState();
+	const [todoList, setTodoList] = React.useState([]);
+
+	const handleSubmit = async () => {
+		setTodoList([{ id: todoList.length + 1, todo }, ...todoList]);
+	};
+
+	const handleEdit = () => {};
+	return (
+		<div style={{ padding: 8 }}>
+			<h1>Add your task...</h1>
+			<section>
+				<input
+					value={todo}
+					type="text"
+					onChange={(e) => setTodo(e.target.value)}
+				/>
+				<button onClick={() => handleSubmit()}>Enviar</button>
+			</section>
+			<section style={{ marginTop: '24px' }}>
+				<h2>Todo List</h2>
+				{todoList.map((el) => (
+					<div
+						key={el.id}
+						style={{
+							border: '1px solid black',
+							width: 500,
+							display: 'flex',
+							justifyContent: 'space-between',
+							alignItems: 'center',
+							padding: 4,
+							margin: 4,
+						}}
+					>
+						<p>{el.todo}</p>
+						<div>
+							<button onClick={() => handleEdit()}>Editar</button>
+							<button>Excluir</button>
+						</div>
+					</div>
+				))}
+			</section>
+		</div>
+	);
 }
 
 export default App;
